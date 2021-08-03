9b82b6cea81c47af130f8a5f224abf3d94b522e9
reidemeister <silvio.pavanetto@gmail.com>
2021-08-03 21:51:34 +0200

    dynamic programming

--- Software_Engineer/dp/can_construct_string.py
+++ Software_Engineer/dp/can_construct_string.py
@@ -1,22 +1,27 @@
 def can_construct(target, word_bank, memo={}):
+    if target in memo:
+        return memo[target]
     if len(target) == 0:
         memo[target] = True
         return True
 
     for word in word_bank:
         if target.startswith(word) and len(word) <= len(target):
-            if target[len(word) :] in memo:
-                result = memo[target[len(word) :]]
-            else:
-                result = can_construct(target[len(word) :], word_bank, memo)
-                memo[target] = result
+            result = can_construct(target[len(word) :], word_bank, memo)
+            memo[target] = result
             if result:
                 return True
 
+    memo[target] = False
     return False
 
 
 if __name__ == "__main__":
     target = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef"
     word_bank = ["e", "ee", "eeee", "eeeeee", "eeeeeeee", "eeeeeeeeee"]
+    import time
+
+    start = time.time()
     print(can_construct(target, word_bank))
+    end = time.time()
+    print(end - start)
