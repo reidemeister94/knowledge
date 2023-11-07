1ff7c92575ff1a39c94645ed7034195552568569
spav <silvio.pavanetto@jobtome.com>
2023-11-07 19:02:13 +0100

    feat: add two pointers problems

---
+++ algorithms/two_pointers/3_sum.py
@@ -0,0 +1,75 @@
+"""
+15. 3Sum
+https://leetcode.com/problems/3sum/description/
+
+Given an integer array nums, return all the triplets
+[nums[i], nums[j], nums[k]] such that
+i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+"""
+from typing import List
+
+
+class Solution:
+    def two_sum(self, nums, idx, target):
+        i = idx
+        j = len(nums) - 1
+
+        while i < j:
+            if nums[i] + nums[j] == target:
+                self.triplets.append((-target, nums[i], nums[j]))
+                while i < j and nums[i] == nums[i + 1]:
+                    i += 1
+                while i < j and nums[j] == nums[j - 1]:
+                    j -= 1
+                i += 1
+                j -= 1
+            elif nums[i] + nums[j] > target:
+                j -= 1
+            elif nums[i] + nums[j] < target:
+                i += 1
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        self.triplets = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            self.two_sum(nums, i + 1, -nums[i])
+
+        return self.triplets
